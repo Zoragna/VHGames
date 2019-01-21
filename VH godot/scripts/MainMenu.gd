59eede3427be2a9b7eaefd7da6c35608d272017f
antoine giego <viessur@discord.com>
2019-01-21 01:06:31 +0100

    lobbying

--- VH godot/scripts/MainMenu.gd
+++ VH godot/scripts/MainMenu.gd
@@ -12,34 +12,29 @@ func _ready():
 	
 	get_node("Menu").visible=true
 	get_node("Options menu").visible=false
-
-func _process(delta):
-	if options_enable==true :
-		get_node("Options menu").visible=true
-		get_node("Menu").visible=false
-	else :
-		get_node("Menu").visible=true
-		get_node("Options menu").visible=false
-
-
-
-func _on_Quit_button_down():
-	get_tree().quit()
-
-
-func _on_Start_button_down():
-	get_tree().change_scene("res://scene/map.tscn")
-
-
-
-func _on_Options_button_down():
-	options_enable=true
-
-
-
-func _on_Retour_button_down():
-	options_enable=false
+	get_node("Lobby").visible = false
 
 
 func _on_Volume_changed():
 	get_node("son").volume_db=get_node("Options menu/Volume").value
+
+
+func _on_Retour_pressed():
+	get_node("Options menu").visible=false
+	get_node("Lobby").visible = false
+	get_node("Menu").visible=true
+
+
+func _on_Start_pressed():
+	get_node("Menu").visible = false
+	get_node("Options menu").visible = false
+	get_node("Lobby").visible = true
+
+
+func _on_Options_pressed():
+	get_node("Menu").visible = false
+	get_node("Options menu").visible = true
+
+
+func _on_Quit_pressed():
+	get_tree().quit()
