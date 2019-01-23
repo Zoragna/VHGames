4493c413ef1f45afc84a798c798e3134918b3de9
La Paella Masquer <sharkshadow@live.fr>
2019-01-23 23:09:17 +0100

    navigation refaite et fixe son

--- VH godot/scripts/MainMenu.gd
+++ VH godot/scripts/MainMenu.gd
@@ -4,38 +4,39 @@ extends Node2D
 # var a = 2
 # var b = "textvar" Menu
 
-var options_enable=false
 var btns
 var current_focused_button
-var down=false
-var up=false
-var right=false
-var left=false
 
+const main_btns = {
+	"Menu/Start":{
+		"down": "Menu/Options",
+		"up":"Menu/Quit" 
+	},
+	"Menu/Options":{
+		"down":"Menu/Quit",
+		"up":"Menu/Start"
+	},
+	"Menu/Quit":{
+		"down":"Menu/Start",
+		"up":"Menu/Options"
+	}
+}
+	
+const options_btns = {
+	"Options menu/Volume":{
+		"down":"Options menu/Retour",
+		"up":"Options menu/Retour"
+	},
+	"Options menu/Retour":{
+		"down":"Options menu/Volume",
+		"up":"Options menu/Volume"
+	}
+}
 
 func _ready():
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
-	
-	
-	btns= {"Menu/Start":{
-		"joy_down": "Menu/Options",
-		"joy_up":"Menu/Quit",
-		"joy_right": "Menu/Start",
-		"joy_left": "Menu/Start" },
-		"Menu/Options":{
-		"joy_down":"Menu/Quit",
-		"joy_up":"Menu/Start",
-		"joy_right": "Menu/Options",
-		"joy_left": "Menu/Options"}	,
-		"Menu/Quit":{
-		"joy_down":"Menu/Start",
-		"joy_up":"Menu/Options",
-		"joy_right": "Menu/Quit",
-		"joy_left": "Menu/Quit"}
-	}
-	
-	
+	btns = main_btns
 	current_focused_button="Menu/Start"
 	get_node(current_focused_button).grab_focus()
 	get_node("Menu").visible=true
@@ -43,13 +44,10 @@ func _ready():
 
 
 func _on_Volume_value_changed(value):
-	get_node("son").volume_db= value-24
-
-
+	get_node("son").volume_db= value-80
 
 func _on_Start_pressed():
 	get_tree().change_scene("res://scene/map.tscn")
-
 
 func _on_Options_pressed():
 	get_node("Menu").visible = false
@@ -57,22 +55,10 @@ func _on_Options_pressed():
 	current_focused_button="Options menu/Volume"
 	get_node(current_focused_button).grab_focus()
 	
-	btns={"Options menu/Volume":{
-		"joy_down":"Options menu/Retour",
-		"joy_up":"Options menu/Retour",
-		"joy_right": "Options menu/Volume",
-		"joy_left": "Options menu/Volume"},
-		"Options menu/Retour":{
-		"joy_down":"Options menu/Volume",
-		"joy_up":"Options menu/Volume",
-		"joy_right": "Options menu/Retour",
-		"joy_left": "Options menu/Retour"}
-		}
-
+	btns = options_btns
 
 func _on_Quit_pressed():
 	get_tree().quit()
-
 
 func _on_Retour_pressed():
 	get_node("Options menu").visible=false
@@ -80,63 +66,61 @@ func _on_Retour_pressed():
 	current_focused_button="Menu/Start"
 	get_node(current_focused_button).grab_focus()
 	
-	btns= {"Menu/Start":{
-		"joy_down": "Menu/Options",
-		"joy_up":"Menu/Quit",
-		"joy_right": "Menu/Start",
-		"joy_left": "Menu/Start" },
-		"Menu/Options":{
-		"joy_down":"Menu/Quit",
-		"joy_up":"Menu/Start",
-		"joy_right": "Menu/Options",
-		"joy_left": "Menu/Options"}	,
-		"Menu/Quit":{
-		"joy_down":"Menu/Start",
-		"joy_up":"Menu/Options",
-		"joy_right": "Menu/Quit",
-		"joy_left": "Menu/Quit"}
-	}
+	btns= main_btns
 
-
-
+func change_button(direction):
+	print(direction)
+	print(current_focused_button)
+	print(btns)
+	print(btns[current_focused_button])
+	print(btns[current_focused_button][direction])
+	current_focused_button = btns[current_focused_button][direction]
+	get_node(current_focused_button).grab_focus()
 
 func _input(event):
-	get_node(current_focused_button).grab_focus()
-	if !down:
-		if (Input.get_joy_axis(0, JOY_ANALOG_LY) >0.3 ) :
-			get_node(btns[current_focused_button]["joy_down"]).grab_focus()
-			current_focused_button=btns[current_focused_button]["joy_down"]
-			down=true
-	elif down:
-		if not (Input.get_joy_axis(0, JOY_ANALOG_LY) >0.3):
-			down=false
-	
-	if !up:
-		if (Input.get_joy_axis(0, JOY_ANALOG_LY) < -0.3 ) :
-			get_node(btns[current_focused_button]["joy_up"]).grab_focus()
-			current_focused_button=btns[current_focused_button]["joy_up"]
-			up=true
-	elif up:
-		if not (Input.get_joy_axis(0, JOY_ANALOG_LY) < -0.3 ) :
-			up=false
-	
-	if !right:
-		if (Input.get_joy_axis(0, JOY_ANALOG_LX) < -0.3 ) :
-			get_node(btns[current_focused_button]["joy_right"]).grab_focus()
-			current_focused_button=btns[current_focused_button]["joy_right"]
-			right=true
-	elif right:
-		if not (Input.get_joy_axis(0, JOY_ANALOG_LX) < -0.3 ) :
-			right=false
-	
-	if !left:
-		if (Input.get_joy_axis(0, JOY_ANALOG_LX) < 0.3 ) :
-			get_node(btns[current_focused_button]["joy_left"]).grab_focus()
-			current_focused_button=btns[current_focused_button]["joy_left"]
-			left=true
-	elif left:
-		if not (Input.get_joy_axis(0, JOY_ANALOG_LX) < 0.3 ) :
-			left=false
+	#get_node(current_focused_button).grab_focus()
+	if event is InputEventKey : #&& !event.is_echo() :
+		if event.is_action_pressed("ui_down"):
+			change_button("down")
+		elif event.is_action_pressed("ui_up"):
+			change_button("up")
+	elif event is InputEventJoypadMotion :
+		if get_node("Timer").is_stopped() :
+			if get_node("Timer/TimerTimer").is_stopped() :
+				if event.axis == JOY_AXIS_1 && event.axis_value > 0.3 :
+					change_button("down")
+					get_node("Timer").start()
+				elif event.axis == JOY_AXIS_1 && event.axis_value < -0.3 :
+					change_button("up")
+					get_node("Timer").start()
+
+func _on_Quit_focus_entered():
+	pass # replace with function body
 
 
+func _on_Quit_focus_exited():
+	pass # replace with function body
 
+
+func _on_Options_focus_exited():
+	pass # replace with function body
+
+
+func _on_Options_focus_entered():
+	pass # replace with function body
+
+
+func _on_Start_focus_entered():
+	pass # replace with function body
+
+
+func _on_Start_focus_exited():
+	pass # replace with function body
+
+
+func _on_Timer_timeout():
+	get_node("Timer/TimerTimer").start()
+
+
+func _on_TimerTimer_timeout():
+	pass # replace with function body
